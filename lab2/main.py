dd138ce05259186471097035a5dcb7ce79e7890c
Vanvik <ivanspecial5@gmail.com>
2022-12-21 18:29:04 +0300

    лаб2

---
+++ lab2/main.py
@@ -0,0 +1,21 @@
+from py_oop.rectangle import Rectangle
+from py_oop.circle import Circle
+from py_oop.square import Square
+
+
+def main():
+    r = Rectangle("синего", 3, 2)
+    c = Circle("зеленого", 5)
+    s = Square("красного", 5)
+    print(r)
+    print(c)
+    print(s)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
